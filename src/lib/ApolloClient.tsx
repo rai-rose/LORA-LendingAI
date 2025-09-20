366e35891cd8e6d5fec99507961a66292d82f007
AyumuRAI <rai.ayumucueva@gmail.com>
2025-09-20 23:06:26 +0800

    Added graphql, apollo client and cross-fetch for graphql queries and mutations to be connected to the server-side

---
+++ src/lib/ApolloClient.tsx
@@ -0,0 +1,12 @@
+import { ApolloClient, InMemoryCache,HttpLink } from "@apollo/client";
+import fetch from "cross-fetch";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://localhost:5000/graphql", // This is temporary endpoint, will be changed in the future
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
