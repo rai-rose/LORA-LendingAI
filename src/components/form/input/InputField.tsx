d5e3751fd3914189b915f1d451c439d0c91489e9
AyumuRAI <rai.ayumucueva@gmail.com>
2025-09-21 01:37:31 +0800

    Added an optional prop 'required' for form validations

--- src/components/form/input/InputField.tsx
+++ src/components/form/input/InputField.tsx
@@ -19,6 +19,7 @@ interface InputProps {
   multiple?: boolean;
   accept?: string; // For file input types
   readOnly?: boolean;
+  required?: boolean;
 }
 
 const Input: FC<InputProps> = ({
@@ -40,6 +41,7 @@ const Input: FC<InputProps> = ({
   multiple,
   accept,
   readOnly = false,
+  required = false
 }) => {
   // Determine input styles based on state (disabled, success, error)
   let inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${className}`;
@@ -73,6 +75,7 @@ const Input: FC<InputProps> = ({
         multiple={multiple}
         accept={accept}
         readOnly={readOnly}
+        required={required}
       />
 
       {/* Optional Hint Text */}
