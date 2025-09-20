b029cd1547d31ea378cd6585e0c6f92d6687efc9
AyumuRAI <rai.ayumucueva@gmail.com>
2025-09-21 01:36:18 +0800

    Fix: apollo localhost server error due to https protocol in localhost connection

--- src/lib/ApolloClient.tsx
+++ src/lib/ApolloClient.tsx
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://localhost:5000/graphql", // This is temporary endpoint, will be changed in the future
+    uri: "http://localhost:5000/graphql", // This is temporary endpoint, will be changed in the future
     fetch,
   }),
   cache: new InMemoryCache(),
